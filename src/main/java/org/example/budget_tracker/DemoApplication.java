63ce82500c8aab595596644924c5af4990f5516b
QJohnson321 <98294763+QJohnson321@users.noreply.github.com>
2026-04-26 10:34:10 -0400

    Add H2 Docker config and seed categories

--- src/main/java/org/example/budget_tracker/DemoApplication.java
+++ src/main/java/org/example/budget_tracker/DemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication {
@@ -12,8 +15,17 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
 
 
-
-
+    }
+    @Bean
+    CommandLineRunner seedCategories(CategoryRepository categoryRepo) {
+        return args -> {
+            if (categoryRepo.count() == 0) {
+                categoryRepo.save(new Category("Food"));
+                categoryRepo.save(new Category("Gas"));
+                categoryRepo.save(new Category("Travel"));
+                categoryRepo.save(new Category("Bills"));
+            }
+        };
     }
 
 
